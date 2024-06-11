d227747371e50b59692720eb8905cbf59cf401b8
bondgh0954 <otengdevops@gmail.com>
2024-06-11 11:40:03 +0200

    adding files or function in vars folder

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -8,5 +8,24 @@ class Docker implements serializable {
         this.script =script
     }
 
-    
+    def buildJar(){
+        script.echo 'building application jar file'
+        script.sh 'mvn package'
+    }
+
+    def buildImage(String imageName){
+        script.echo "building docker image from the application"
+        script.sh "docker build -t $imageName ."
+    }
+
+    def dockerLogin(){
+        script.echo 'logging to docker private repository'
+        script.withCredentials([script.usernamePassword(credentialsId:'dockerhub-credentials',usernameVariable:'USERNAME',passwordVariable:'PASSWORD')]){
+            script.sh "echo '${PASSWORD}' |docker login -u '${USERNAME}' --password-stdin"
+        }
+    }
+    def dockerPush(String imageName){
+        script.echo 'pushing image into the private repository'
+        script.sh "docker push $imageName"
+    }
 }
